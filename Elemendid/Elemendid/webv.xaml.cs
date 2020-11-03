8094a01060fe4e4ff7502b3a41de3bf3ff934ee6
Nicole <aasakuyla@gmail.com>
2020-11-03 12:00:46 +0200

    make webv

--- Elemendid/Elemendid/webv.xaml.cs
+++ Elemendid/Elemendid/webv.xaml.cs
@@ -16,28 +16,4 @@ namespace Elemendid
         WebView webView;
         Entry urlEntry;
         public webv()
-        {
-            urlEntry = new Entry { HorizontalOptions = LayoutOptions.FillAndExpand };
-            Button button = new Button { Text = "Go" };
-            button.Clicked += button_Clicked;
-            StackLayout stack = new StackLayout
-            {
-                Orientation = StackOrientation.Horizontal,
-                Children = { button, urlEntry }
-            };
-            webView = new WebView
-            {
-                Source = new UrlWebViewSource { Url = "https://www.tthk.ee/" },
-
-                VerticalOptions = LayoutOptions.FillAndExpand
-            };
-
-            this.Content = new StackLayout { Children = { stack, webView } };
-        }
-
-        void button_Clicked(object sender, EventArgs e)
-        {
-            webView.Source = new UrlWebViewSource { Url = urlEntry.Text };
-        }
-    }
-}
+        
