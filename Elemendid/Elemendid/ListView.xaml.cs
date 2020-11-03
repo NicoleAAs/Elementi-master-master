f761c64fea098866aaabc01d8fc31615f0ac7659
Nicole <aasakuyla@gmail.com>
2020-11-03 11:59:52 +0200

    Add project files.

---
+++ Elemendid/Elemendid/ListView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Elemendid
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListView : ContentPage
+    {
+        public ListView()
+        {
+            StackLayout st = new StackLayout();
+            Button tagasi_btn = new Button { Text = "Back" };
+            tagasi_btn.HorizontalOptions = LayoutOptions.StartAndExpand;
+            tagasi_btn.VerticalOptions = LayoutOptions.EndAndExpand;
+            tagasi_btn.Clicked += Tagasi_btn_Clicked;
+            st.Children.Add(tagasi_btn);
+            Content = st;
+        }
+
+        private async void Tagasi_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
